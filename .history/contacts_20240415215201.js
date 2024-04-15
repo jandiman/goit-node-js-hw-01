34a6aa28a563eb455b343339787ff91ecb5f86ad
Jan Dilbert Ampuan <92305240+jandiman@users.noreply.github.com>
2024-04-15 22:39:57 +0800

    completed codes for node.js hw01

---
+++ .history/contacts_20240415215201.js
@@ -0,0 +1,5 @@
+import fs from "fs/promises";
+import path from "path";
+import { nanoid } from "nanoid";
+
+const contactsPath = pah.join("db", "contacts.json");
---
+++ .history/contacts_20240415215304.js
@@ -0,0 +1,7 @@
+import fs from "fs/promises";
+import path from "path";
+import { nanoid } from "nanoid";
+
+const contactsPath = pah.join("db", "contacts.json");
+
+const listContacts =  
